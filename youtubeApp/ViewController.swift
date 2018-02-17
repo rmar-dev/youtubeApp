4965799b2f00630898d2926bbe032b3407268d43
Ricardo Rabeto <ricardomarabeto@gmail.com>
2018-02-17 09:04:42 +0000

    Initial Commit

---
+++ youtubeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtubeApp
+//
+//  Created by Ricardo Rabeto on 17/02/2018.
+//  Copyright © 2018 Ricardo Rabeto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
